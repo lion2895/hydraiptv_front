a5302be0832344725bd6903e5626e8073db5ab76
ayoub <lionayoub50@gmail.com>
2022-10-28 16:46:20 +0100

    fix margin issues

--- pages/cart.vue
+++ pages/cart.vue
@@ -17,100 +17,83 @@
 
       <div>
         <vs-row :w="12" class="mb-5" justify="center">
-          <vs-card type="4" class="mr-4  mb-4">
-            <template #title>
-              <vs-row class="mb-3" :w="12">
-                <vs-col :w="7">
-                  <h3>44.95 EUR</h3>
-                </vs-col>
-                <vs-col :w="5"> <vs-switch  color="#ff448a"  v-model="adultestate" >
-                  <template #off>
-                    No Adult
-                  </template>
-                  <template #on>
-                    with Adult
-                  </template>
-                </vs-switch></vs-col>
-              </vs-row>
+
+            <vs-card type="4" class="mr-4  mb-4">
+              <template #title>
+                <vs-row class="mb-3" :w="12">
+                  <vs-col :w="6">
+                    <h3>44.95 EUR</h3>
+                  </vs-col>
+                  <vs-col :w="5"> <vs-switch  color="#ff448a"  v-model="adultestate" >
+                    <template #off>
+                      No Adult
+                    </template>
+                    <template #on>
+                      with Adult
+                    </template>
+                  </vs-switch></vs-col>
+                </vs-row>
 
 
 
-            </template>
-            <template #img>
+              </template>
+              <template #img>
 
-              <img src="https://vuesax.com/foto6.png" alt="">
+                <img src="https://vuesax.com/foto6.png" alt="">
 
 
-            </template>
-            <template #text >
-              <vs-row :w="12" align="center">
-                <vs-col :w="4">
-                   Devices
-                </vs-col>
-                <vs-col class="mb-3" :w="8">
-                  <vs-select   placeholder="Select a Device" v-model="selectvalue">
-                    <vs-option label="Vuesax" value="1">
-                      Vuesax
-                    </vs-option>
-                    <vs-option label="Vue" value="2">
-                      Vue
-                    </vs-option>
-                    <vs-option label="Javascript" value="3">
-                      Javascript
-                    </vs-option>
-                    <vs-option disabled label="Sass" value="4">
-                      Sass
-                    </vs-option>
-                    <vs-option label="Typescript" value="5">
-                      Typescript
-                    </vs-option>
-                    <vs-option label="Webpack" value="6">
-                      Webpack
-                    </vs-option>
-                    <vs-option label="Nodejs" value="7">
-                      Nodejs
-                    </vs-option>
-                  </vs-select>
-                </vs-col>
-                <vs-col :w="4">
+              </template>
+              <template #text >
+                <vs-row :w="12" align="center">
+                  <vs-col :w="3">
+                    Devices
+                  </vs-col>
+                  <vs-col class="mb-3" :w="8">
+                    <vs-select   placeholder="Select a Device" v-model="selectvalue">
+                      <vs-option label="Vuesax" value="1">
+                        Vuesax
+                      </vs-option>
+                      <vs-option label="Vue" value="2">
+                        Vue
+                      </vs-option>
+                      <vs-option label="Javascript" value="3">
+                        Javascript
+                      </vs-option>
+                      <vs-option disabled label="Sass" value="4">
+                        Sass
+                      </vs-option>
+                      <vs-option label="Typescript" value="5">
+                        Typescript
+                      </vs-option>
+                      <vs-option label="Webpack" value="6">
+                        Webpack
+                      </vs-option>
+                      <vs-option label="Nodejs" value="7">
+                        Nodejs
+                      </vs-option>
+                    </vs-select>
+                  </vs-col>
+                  <vs-col :w="3">
                     Quantity
-                </vs-col>
-                <vs-col   :w="6" align="center">
-                  <vs-input
-                    primary
+                  </vs-col>
+                  <vs-col   :w="6" >
+                    <vs-input
+                      primary
 
-                    type="number"
+                      type="number"
 
-                    placeholder="Quantity" />
+                      placeholder="Quantity" />
 
-                </vs-col>
-
-
-              </vs-row>
+                  </vs-col>
 
 
-
-
-
-            </template>
+                </vs-row>
 
 
 
 
 
-
-
-            <template #interactions style="width: -webkit-fill-available;">
-
-              <vs-button danger icon>
-                <v-icon v-text="'mdi-close-octagon'"></v-icon>
-              </vs-button>
-              <vs-button class="btn-chat" shadow primary>
-
-                <span class="span">
-          12 month
-        </span>
-              </vs-button>
+              </template>
 
 
 
@@ -118,107 +101,17 @@
 
 
 
+              <template #interactions style="width: -webkit-fill-available;">
 
-
-
-
-            </template>
-
-          </vs-card>
-          <vs-card type="4" class="mr-4  mb-4">
-            <template #title>
-              <vs-row class="mb-3" :w="12">
-                <vs-col :w="7">
-                  <h3>44.95 EUR</h3>
-                </vs-col>
-                <vs-col :w="5"> <vs-switch  color="#ff448a"  v-model="adultestate" >
-                  <template #off>
-                    No Adult
-                  </template>
-                  <template #on>
-                    with Adult
-                  </template>
-                </vs-switch></vs-col>
-              </vs-row>
-
-
-
-            </template>
-            <template #img>
-
-              <img src="https://vuesax.com/foto6.png" alt="">
-
-
-            </template>
-            <template #text >
-              <vs-row :w="12" align="center">
-                <vs-col :w="4">
-                  Devices
-                </vs-col>
-                <vs-col class="mb-3" :w="8">
-                  <vs-select   placeholder="Select a Device" v-model="selectvalue">
-                    <vs-option label="Vuesax" value="1">
-                      Vuesax
-                    </vs-option>
-                    <vs-option label="Vue" value="2">
-                      Vue
-                    </vs-option>
-                    <vs-option label="Javascript" value="3">
-                      Javascript
-                    </vs-option>
-                    <vs-option disabled label="Sass" value="4">
-                      Sass
-                    </vs-option>
-                    <vs-option label="Typescript" value="5">
-                      Typescript
-                    </vs-option>
-                    <vs-option label="Webpack" value="6">
-                      Webpack
-                    </vs-option>
-                    <vs-option label="Nodejs" value="7">
-                      Nodejs
-                    </vs-option>
-                  </vs-select>
-                </vs-col>
-                <vs-col :w="4">
-                  Quantity
-                </vs-col>
-                <vs-col   :w="6" align="center">
-                  <vs-input
-                    primary
-
-                    type="number"
-
-                    placeholder="" />
-
-                </vs-col>
-
-
-              </vs-row>
-
-
-
-
-
-            </template>
-
-
-
-
-
-
-
-            <template #interactions style="width: -webkit-fill-available;">
-
-              <vs-button danger icon>
-                <v-icon v-text="'mdi-close-octagon'"></v-icon>
-              </vs-button>
-              <vs-button class="btn-chat" shadow primary>
+                <vs-button danger icon>
+                  <v-icon v-text="'mdi-close-octagon'"></v-icon>
+                </vs-button>
+                <vs-button class="btn-chat" shadow primary>
 
                 <span class="span">
           12 month
         </span>
-              </vs-button>
+                </vs-button>
 
 
 
@@ -230,103 +123,103 @@
 
 
 
-            </template>
+              </template>
 
-          </vs-card>
-          <vs-card type="4" class="mr-4  mb-4">
-            <template #title>
-              <vs-row class="mb-3" :w="12">
-                <vs-col :w="7">
-                  <h3>44.95 EUR</h3>
-                </vs-col>
-                <vs-col :w="5"> <vs-switch  color="#ff448a"  v-model="adultestate" >
-                  <template #off>
-                    No Adult
-                  </template>
-                  <template #on>
-                    with Adult
-                  </template>
-                </vs-switch></vs-col>
-              </vs-row>
-
-
-
-            </template>
-            <template #img>
-
-              <img src="https://vuesax.com/foto6.png" alt="">
-
-
-            </template>
-            <template #text >
-              <vs-row :w="12" align="center">
-                <vs-col :w="4">
-                  Devices
-                </vs-col>
-                <vs-col class="mb-3" :w="8">
-                  <vs-select   placeholder="Select a Device" v-model="selectvalue">
-                    <vs-option label="Vuesax" value="1">
-                      Vuesax
-                    </vs-option>
-                    <vs-option label="Vue" value="2">
-                      Vue
-                    </vs-option>
-                    <vs-option label="Javascript" value="3">
-                      Javascript
-                    </vs-option>
-                    <vs-option disabled label="Sass" value="4">
-                      Sass
-                    </vs-option>
-                    <vs-option label="Typescript" value="5">
-                      Typescript
-                    </vs-option>
-                    <vs-option label="Webpack" value="6">
-                      Webpack
-                    </vs-option>
-                    <vs-option label="Nodejs" value="7">
-                      Nodejs
-                    </vs-option>
-                  </vs-select>
-                </vs-col>
-                <vs-col :w="4">
-                  Quantity
-                </vs-col>
-                <vs-col   :w="6" align="center">
-                  <vs-input
-                    primary
-
-                    type="number"
-
-                     />
-
-                </vs-col>
-
-
-              </vs-row>
+            </vs-card>
+            <vs-card type="4" class="mr-4  mb-4">
+              <template #title>
+                <vs-row class="mb-3" :w="12">
+                  <vs-col :w="7">
+                    <h3>44.95 EUR</h3>
+                  </vs-col>
+                  <vs-col :w="5"> <vs-switch  color="#ff448a"  v-model="adultestate" >
+                    <template #off>
+                      No Adult
+                    </template>
+                    <template #on>
+                      with Adult
+                    </template>
+                  </vs-switch></vs-col>
+                </vs-row>
 
 
 
+              </template>
+              <template #img>
+
+                <img src="https://vuesax.com/foto6.png" alt="">
 
 
-            </template>
+              </template>
+              <template #text >
+                <vs-row :w="12" align="center">
+                  <vs-col :w="4">
+                    Devices
+                  </vs-col>
+                  <vs-col class="mb-3" :w="8">
+                    <vs-select   placeholder="Select a Device" v-model="selectvalue">
+                      <vs-option label="Vuesax" value="1">
+                        Vuesax
+                      </vs-option>
+                      <vs-option label="Vue" value="2">
+                        Vue
+                      </vs-option>
+                      <vs-option label="Javascript" value="3">
+                        Javascript
+                      </vs-option>
+                      <vs-option disabled label="Sass" value="4">
+                        Sass
+                      </vs-option>
+                      <vs-option label="Typescript" value="5">
+                        Typescript
+                      </vs-option>
+                      <vs-option label="Webpack" value="6">
+                        Webpack
+                      </vs-option>
+                      <vs-option label="Nodejs" value="7">
+                        Nodejs
+                      </vs-option>
+                    </vs-select>
+                  </vs-col>
+                  <vs-col :w="4">
+                    Quantity
+                  </vs-col>
+                  <vs-col   :w="6" align="center">
+                    <vs-input
+                      primary
+
+                      type="number"
+
+                      placeholder="" />
+
+                  </vs-col>
+
+
+                </vs-row>
 
 
 
 
 
+              </template>
 
 
-            <template #interactions style="width: -webkit-fill-available;">
 
-              <vs-button danger icon>
-                <v-icon v-text="'mdi-close-octagon'"></v-icon>
-              </vs-button>
-              <vs-button class="btn-chat" shadow primary>
+
+
+
+
+              <template #interactions style="width: -webkit-fill-available;">
+
+                <vs-button danger icon>
+                  <v-icon v-text="'mdi-close-octagon'"></v-icon>
+                </vs-button>
+                <vs-button class="btn-chat" shadow primary>
 
                 <span class="span">
           12 month
         </span>
-              </vs-button>
+                </vs-button>
 
 
 
@@ -338,27 +231,144 @@
 
 
 
-            </template>
+              </template>
 
-          </vs-card>
+            </vs-card>
+            <vs-card type="4" class="mr-4  mb-4">
+              <template #title>
+                <vs-row class="mb-3" :w="12">
+                  <vs-col :w="7">
+                    <h3>44.95 EUR</h3>
+                  </vs-col>
+                  <vs-col :w="5"> <vs-switch  color="#ff448a"  v-model="adultestate" >
+                    <template #off>
+                      No Adult
+                    </template>
+                    <template #on>
+                      with Adult
+                    </template>
+                  </vs-switch></vs-col>
+                </vs-row>
+
+
+
+              </template>
+              <template #img>
+
+                <img src="https://vuesax.com/foto6.png" alt="">
+
+
+              </template>
+              <template #text >
+                <vs-row :w="12" align="center">
+                  <vs-col :w="4">
+                    Devices
+                  </vs-col>
+                  <vs-col class="mb-3" :w="8">
+                    <vs-select   placeholder="Select a Device" v-model="selectvalue">
+                      <vs-option label="Vuesax" value="1">
+                        Vuesax
+                      </vs-option>
+                      <vs-option label="Vue" value="2">
+                        Vue
+                      </vs-option>
+                      <vs-option label="Javascript" value="3">
+                        Javascript
+                      </vs-option>
+                      <vs-option disabled label="Sass" value="4">
+                        Sass
+                      </vs-option>
+                      <vs-option label="Typescript" value="5">
+                        Typescript
+                      </vs-option>
+                      <vs-option label="Webpack" value="6">
+                        Webpack
+                      </vs-option>
+                      <vs-option label="Nodejs" value="7">
+                        Nodejs
+                      </vs-option>
+                    </vs-select>
+                  </vs-col>
+                  <vs-col :w="4">
+                    Quantity
+                  </vs-col>
+                  <vs-col   :w="6" align="center">
+                    <vs-input
+                      primary
+
+                      type="number"
+
+                    />
+
+                  </vs-col>
+
+
+                </vs-row>
+
+
+
+
+
+              </template>
+
+
+
+
+
+
+
+              <template #interactions style="width: -webkit-fill-available;">
+
+                <vs-button danger icon>
+                  <v-icon v-text="'mdi-close-octagon'"></v-icon>
+                </vs-button>
+                <vs-button class="btn-chat" shadow primary>
+
+                <span class="span">
+          12 month
+        </span>
+                </vs-button>
+
+
+
+
+
+
+
+
+
+
+
+              </template>
+
+            </vs-card>
+
+
+
+          <vs-col :w="12" class="mr-4">
+            <v-simple-table ju class="mb-8">
+              <template v-slot:default>
+                <thead>
+                <tr>
+                  <th class="text-left">
+                    TOTAL
+                  </th>
+                  <th class="text-right" >
+                    22.5 EUR
+                  </th>
+                </tr>
+                </thead>
+              </template>
+            </v-simple-table>
+          </vs-col>
+
+
+
 
 
         </vs-row>
 
-          <v-simple-table class="mb-8 mr-4">
-            <template v-slot:default>
-              <thead>
-              <tr>
-                <th class="text-left">
-                  TOTAL
-                </th>
-                <th class="text-right" >
-                  22.5 EUR
-                </th>
-              </tr>
-              </thead>
-            </template>
-          </v-simple-table>
+
 
       </div>
 
